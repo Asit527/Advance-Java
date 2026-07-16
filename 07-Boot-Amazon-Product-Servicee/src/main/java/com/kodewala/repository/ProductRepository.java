740c45396db0b9f06f8da9e934a1b2f1bc8c5dcd
Asit Kumar Nayak <asitkumarnayak630@gmail.com>
2026-07-17 01:45:03 +0530

    new project

--- 07-Boot-Amazon-Product-Servicee/src/main/java/com/kodewala/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kodewala.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.kodewala.entity.ProductEntity;
-
-public interface ProductRepository extends MongoRepository<ProductEntity,Long> {
-
-}
